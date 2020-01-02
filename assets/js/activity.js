874b1598320d2d82fc4e24be7743b9eba92b64a3
sjmars <sjmarsnc@gmail.com>
2020-01-02 12:53:19 -0500

    added modal code back in

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -6,6 +6,25 @@ function displayResults() {
 
   var city = $(".cityInput").val().trim();
 
+  if (city === '') {
+    // Get the modal
+  var modal = document.getElementById("myModal");
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  modal.style.display = "block";
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  }
+
   var weatherAPIKey = "1e2baac35502ada6a2f1228a812729b7";
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIKey;
 
